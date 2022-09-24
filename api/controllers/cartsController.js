1630e4f33c0ed1cc42485dc9ea9bf95d020cde31
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.3>
2022-09-24 19:17:41 -0300

    creando la funcion updateCart

--- api/controllers/cartsController.js
+++ api/controllers/cartsController.js
@@ -35,36 +35,26 @@ const controllerCart = {
         });
     }
   },
-  updateCart: (req, res) => {
-    const id = req.params.id;
+  updateCart: async (req, res) => {
     const dataUpdate = req.body;
 
     try {
-      const cartDBJson = fs.readFileSync("api/data/carts.json", "utf-8");
-      let cartDB = JSON.parse(cartDBJson);
+      const cartDBJson = await models.product_cart.update( dataUpdate, {
+        where: {id: req.params.id}
+      } )
 
-      let cart = cartDB.find(el => el.user === Number(id));
-      if (cart){
-        cartDB.forEach(el => {
-            if(el.user === Number(id)){
-                el.cart = dataUpdate;
-            }
-        });
-
-        cart = cartDB.find(el => el.user === Number(id))
-
-        fs.writeFileSync("api/data/carts.json", JSON.stringify(cartDB));
-
-        res.status(200).send(cart.cart)
+      if (cartDBJson){
+        res.status(200).json(cartDBJson)
       } else {
         res.status(404).json({
-            msg: 'Not Found'
+            ok: false,
+            message: 'Not Found'
         })
       }
     } catch (error) {
         console.log(error);
         res.status(500).json({
-           msg: 'Error interno'
+           message: 'Error interno'
         });
     }
   }
