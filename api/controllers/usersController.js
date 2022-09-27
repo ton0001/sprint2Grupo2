87bccdec74a6efe3d757846eba50bcf64abd3b40
luuroldan <arq.rodriguezroldan@gmail.com>
2022-09-27 10:15:32 -0300

    deleteUser Actualizado

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,6 +1,8 @@
-const { generateJWT } = require("../../helpers/generateJWT");
-const initModels = require("../../database/models/init-models");
-const { sequelize } = require("../../database/models");
+const { generateJWT } = require('../../helpers/generateJWT');
+const initModels = require('../../database/models/init-models');
+const { sequelize } = require('../../database/models');
+const carts = require('../../database/models/carts');
+const users = require('../../database/models/users');
 const models = initModels(sequelize);
 
 /*recupero la lista de usuarios, respondo con un array conteniendo 
@@ -11,35 +13,38 @@ const getUsers = async (req, res) => {
       include: [
         {
           model: models.carts,
-          as: "carts",
-          attributes: { exclude: ["user_id"] },
+          as: 'carts',
+          attributes: { exclude: ['user_id'] },
           include: [
             {
               model: models.product_cart,
-              as: "product_carts",
+              as: 'product_carts',
               attributes: [
-                "product_id",
-                "quantity",
-                "created_at",
-                "updated_at",
+                'product_id',
+                'quantity',
+                'created_at',
+                'updated_at',
               ],
             },
           ],
         },
       ],
     });
-    res.send(users);
+    res.json(users);
   } catch (error) {
     console.log(error);
-    res.status(500).send({ message: "Error al obtener los usuarios" });
+    res.status(500).json({ message: 'Error al obtener los usuarios' });
     const usersArray = JSON.parse(users);
-    res.send(usersArray);
+    res.json(usersArray);
   }
 };
 
 /*recupero el user con el id solicitado. 
 Responde con la informacion completa del usuario solicitado
-si no existe, respondo con un mensaje de error */
+si no existe, respondo con un mensaje de error 
+200 OK.
+404 Not Found (si el user no existe)
+500 Server Error.*/
 
 const getUserById = async (req, res) => {
   try {
@@ -48,17 +53,17 @@ const getUserById = async (req, res) => {
       include: [
         {
           model: models.carts,
-          as: "carts",
-          attributes: { exclude: ["user_id"] },
+          as: 'carts',
+          attributes: { exclude: ['user_id'] },
           include: [
             {
               model: models.product_cart,
-              as: "product_carts",
+              as: 'product_carts',
               attributes: [
-                "product_id",
-                "quantity",
-                "created_at",
-                "updated_at",
+                'product_id',
+                'quantity',
+                'created_at',
+                'updated_at',
               ],
             },
           ],
@@ -66,23 +71,26 @@ const getUserById = async (req, res) => {
       ],
     });
     res.status(200).json(user);
+    if (!user) {
+      res.status(404).json({ message: 'Usuario no encontrado' });
+    }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error al obtener el usuario" });
+    res.status(500).json({ message: 'Error al obtener el usuario' });
   }
 };
 
 /*Creo un nuevo Usuario. Debe recibir un body con la informacion del usuario a crear. 
-Responde con la indormacion completa del usuario creado*/
+Responde con la informacion completa del usuario creado*/
 
 const createUser = (req, res) => {
   if (!estanLosDatos(req.body)) {
-    res.status(401).json({ message: "Faltan datos para crear el usuario" });
+    res.status(401).json({ message: 'Faltan datos para crear el usuario' });
   } else {
     try {
       let users = fs.readFileSync(
-        path.join(__dirname, "../data/users.json"),
-        "utf-8"
+        path.join(__dirname, '../data/users.json'),
+        'utf-8'
       );
       users = JSON.parse(users);
       const newUser = {
@@ -97,12 +105,12 @@ const createUser = (req, res) => {
       };
       users.push(newUser);
       fs.writeFileSync(
-        path.join(__dirname, "../data/users.json"),
+        path.join(__dirname, '../data/users.json'),
         JSON.stringify(users)
       );
       res.send(newUser);
     } catch (error) {
-      res.status(500).json({ message: "Error al crear el usuario" });
+      res.status(500).json({ message: 'Error al crear el usuario' });
     }
   }
 };
@@ -125,39 +133,65 @@ const updateUser = async (req, res) => {
       });
       res.status(200).json(updatedUser);
     } else if (!user) {
-      res.status(400).json({ message: "Solicitud Incorrecta" });
+      res.status(400).json({ message: 'Solicitud Incorrecta' });
     } else {
-      res.status(404).json({ message: "Usuario no encontrado" });
+      res.status(404).json({ message: 'Usuario no encontrado' });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error al actualizar el usuario" });
+    res.status(500).json({ message: 'Error al actualizar el usuario' });
   }
 };
 
 /*Elimina un usuario identificado con id. Cuando se elimina un usuario, 
-antes debe vaciarse su carrito. Responde con informacion sobre la eliminacion realizada.
+antes debe verificar vaciarse su carrito. Responde con informacion sobre la eliminacion realizada.
 200 OK.
 400 Bad Request (si la llamada es incorrecta)
 404 Not Found (si el user no existe)
 500 Server Error.*/
 
-const deleteUser = async (req, res) => {
+const deleteUserById = async (req, res) => {
   try {
     const user = await models.users.findOne({
       where: { id: req.params.id },
+      include: [
+        {
+          model: models.carts,
+          as: 'carts',
+          include: [
+            {
+              model: models.product_cart,
+              as: 'product_carts',
+              attributes: [
+                'product_id',
+                'quantity',
+                'created_at',
+                'updated_at',
+              ],
+            },
+          ],
+        },
+      ],
     });
-    if (user) {
-      const deletedUser = await models.users.destroy({
-        where: { id: req.params.id },
+    if (!user) {
+      res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    console.log('PRODUCTS:', user.carts.product_carts.length);
+    if (user.carts.product_carts.length == 0) {
+      await models.carts.destroy({
+        where: { user_id: req.params.id },
       });
-      res.status(200).json(deletedUser);
+      await user.destroy();
+      res.status(200).json({ message: 'Usuario eliminado!!!!' });
     } else {
-      res.status(404).json({ message: "Usuario no encontrado" });
+      res.status(400).json({
+        message: 'No se puede eliminar un usuario con productos en el carrito',
+      });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error al eliminar el usuario" });
+    res.status(500).json({ message: 'Error al eliminar el usuario' });
   }
 };
 
@@ -165,8 +199,8 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const ruta = path.join(__dirname, "..", "data", "users.json");
-    const dbUsers = fs.readFileSync(ruta, "utf-8");
+    const ruta = path.join(__dirname, '..', 'data', 'users.json');
+    const dbUsers = fs.readFileSync(ruta, 'utf-8');
     const users = JSON.parse(dbUsers);
 
     const user = users.find((user) => {
@@ -176,7 +210,7 @@ const login = async (req, res) => {
     if (!user) {
       return res.status(400).json({
         ok: false,
-        mgs: "User NOT found",
+        mgs: 'User NOT found',
       });
     }
 
@@ -189,7 +223,7 @@ const login = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      message: "Authorized",
+      message: 'Authorized',
       user: userLog,
       token,
     });
@@ -197,7 +231,7 @@ const login = async (req, res) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      mgs: "Error login",
+      mgs: 'Error login',
     });
   }
 };
@@ -207,22 +241,6 @@ module.exports = {
   getUserById,
   createUser,
   updateUser,
-  deleteUser,
+  deleteUserById,
   login,
-};
-
-const estanLosDatos = (campos) => {
-  let ret = true;
-  if (
-    !campos.email ||
-    !campos.username ||
-    !campos.password ||
-    !campos.firstname ||
-    !campos.lastname ||
-    !campos.profilepic ||
-    !campos.role
-  ) {
-    ret = false;
-  }
-  return ret;
 };
