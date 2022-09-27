59988bfa181428519247797212cd9f85959f746c
Juan Gabriel Fagundez <juangabrielfagundez@192.168.1.3>
2022-09-27 01:32:38 -0300

    ahora si termine la funcion updateCart

--- api/controllers/cartsController.js
+++ api/controllers/cartsController.js
@@ -12,7 +12,7 @@ const controllerCart = {
           include: [
             {
               model: models.product_cart,
-              as: 'cart_products',
+              as: 'product_carts',
               attributes: ['product_id', 'quantity', 'created_at', 'updated_at']
             }
           ]
@@ -36,65 +36,140 @@ const controllerCart = {
     }
   },
   updateCart: async (req, res) => {
-    const dataUpdate = req.body;
+    let productDB = {};
+    let noStock = false;
 
-    req.body.forEach(product => {
-      try{
-        let productDB = models.products.findByPk(product.product_id);
+    try {
+      // Obtengo el id del cart
+      let cart_ID = await models.carts.findOne({
+        where: {
+          user_id: req.params.id
+        }
+      })
+      
+      //Evaluo si existe el cart_ID
+      if (!cart_ID){
+        return res.status(404).json({
+            ok: false,
+            message: 'El usuario buscado no existe'
+        })
+      } else {
+        //Guardo el id del cart
+        cart_ID = cart_ID.dataValues.id
+      }
 
-        if(productDB.stock < product.quantity) {
+      //Obtengo los productos del cart
+      let cartDB = await models.product_cart.findAll(
+        {
+          where: {cart_id: cart_ID}
+        }
+      )      
+
+      //Agrego el stock del carrito anterior a la db
+      let stockQuantity = {};
+      let plusQuantity = 0;
+      if(cartDB.length > 0){
+        cartDB.forEach(async (product) =>{
+          stockQuantity = await models.products.findByPk(product.dataValues.product_id)
+          plusQuantity = product.dataValues.quantity + stockQuantity.dataValues.stock;
+          console.log(plusQuantity)
+          await models.products.update({stock: plusQuantity},
+            {
+              where: {
+                id: product.dataValues.product_id
+              }
+            });
+          console.log((await models.products.findByPk(product.dataValues.product_id, {
+            attributes: ['id', 'stock']
+          })).dataValues)
+        })  
+      }
+
+      //Evaluo si hay stock para crear el proximo carrito
+      req.body.forEach(async (product) => {
+        productDB = await models.products.findByPk(product.product_id);
+
+        if(productDB.dataValues.stock < product.quantity) {
+          console.log('entro')
+          noStock = true;
+        }
+      })
+
+      //Espero un tiempo para que cargue la base de datos?
+      setTimeout(async ()=>{
+        //Envio respustas segun corresponda
+        if(noStock){
+          //Quito el stock del carrito anterior a la db
+          cartDB.forEach(async (product) =>{
+            stockQuantity = await models.products.findByPk(product.dataValues.product_id)
+            await models.products.update({stock: stockQuantity.dataValues.stock - product.dataValues.quantity },
+              {
+                where: {
+                  id: product.dataValues.product_id
+                }
+              });
+          })  
+          
           res.status(400).json({
             ok: false,
             message: 'No hay stock suficiente'
           })
-        }
-      } catch(err) {
-        return res.status(500).json({
-          ok:false,
-          message: 'Error interno'
-        });
-      }
-    })
-
-    try {
-      const cartDB = await models.product_cart.update( req.body, {
-        where: {id: req.params.id}
-      } )
-
-      if (cartDB){
-        res.status(200).json(cartDB)
-      } else {
-        res.status(404).json({
-            ok: false,
-            message: 'Not Found'
-        })
-      }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-          ok:false,
-           message: 'Error interno'
-        });
-    }
-
-    req.body.forEach(product => {
-      try{
-        let productDB = models.products.findByPk(product.product_id);
-
-        if(productDB.stock >= product.quantity) {
-          models.products.update(product.quantity, {
+        }else {
+          //Borro el carrito anterior
+          await models.product_cart.destroy({
             where: {
-              id: product.product_id
+              cart_id: cart_ID
             }
           })
+  
+          //Creo el carrido nuevo con los datos del body       
+          req.body.forEach(async (product) => {
+            await models.product_cart.create({
+              cart_id: cart_ID,
+              product_id: product.product_id,
+              quantity: product.quantity
+            })
+          })
+  
+          //Actualizo el stock del carrito quitando los datos nuevos
+          let stockQuantityNew = {};
+          let plusQuantityNew = 0;
+          req.body.forEach(async (product) =>{
+            stockQuantityNew = await models.products.findByPk(product.product_id);
+            plusQuantityNew = stockQuantityNew.dataValues.stock - product.quantity;
+            console.log(plusQuantityNew);
+            await models.products.update({stock: plusQuantityNew},
+              {
+                where: {
+                  id: product.product_id
+                }
+              });
+            console.log((await models.products.findByPk(product.product_id, {
+              attributes: ['id', 'stock']
+            })).dataValues)
+          })  
+          
+          //Obtengo los productos del cart actualizado
+          let cartDBupdate = {}
+          setTimeout(async ()=>{
+            cartDBupdate = await models.product_cart.findAll(
+              {
+                where: {cart_id: cart_ID},
+                attributes: ['product_id', 'quantity', 'created_at', 'updated_at']
+              }
+            ) 
+            res.status(200).json(cartDBupdate)
+          }, 30)
         }
-      } catch(err) {
-        res.status(500).json({
-          ok:false,
-          message: 'Error interno'
-        });
-      }
-    })
+      }, 75)
+    } catch (err){
+      console.log(err)
+      res.status(500).json({
+        ok:false,
+        message: 'Error interno del servidor'
+      });
+    }
+    
   }
 };
 
--- database/models/init-models.js
+++ database/models/init-models.js
@@ -14,16 +14,16 @@ function initModels(sequelize) {
   const products = _products(sequelize, DataTypes);
   const users = _users(sequelize, DataTypes);
 
-  product_cart.belongsTo(carts, { as: "cart", foreignKey: "cart_id"});
-  carts.hasMany(product_cart, { as: "cart_products", foreignKey: "cart_id"});
-  carts.belongsTo(users, { as: "user", foreignKey: "user_id"});
-  users.hasOne(carts, { as: "cart", foreignKey: "user_id"});
+  product_cart.belongsTo(carts, { as: "carts", foreignKey: "cart_id"});
+  carts.hasMany(product_cart, { as: "product_carts", foreignKey: "cart_id"});
+  carts.belongsTo(users, { as: "users", foreignKey: "user_id"});
+  users.hasOne(carts, { as: "carts", foreignKey: "user_id"});
   products.belongsTo(category, { as: "category", foreignKey: "category_id"});
   category.hasMany(products, { as: "products", foreignKey: "category_id"});
   pictures.belongsTo(products, { as: "product", foreignKey: "product_id"});
   products.hasMany(pictures, { as: "pictures", foreignKey: "product_id"});
   product_cart.belongsTo(products, { as: "product", foreignKey: "product_id"});
-  products.hasMany(product_cart, { as: "cart", foreignKey: "product_id"});
+  products.hasMany(product_cart, { as: "product_carts", foreignKey: "product_id"});
 
   return {
     carts,
